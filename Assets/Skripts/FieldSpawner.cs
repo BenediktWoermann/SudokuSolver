6209dd915c811b1bf4fb0bfdbe991e09e584b991
woerwolf <benedikt1.woermann@gmail.com>
2019-10-17 08:39:31 +0200

    first working version, but only with around 4-6 numbers left, else the amount of possible brute force answers is to big

--- Assets/Skripts/FieldSpawner.cs
+++ Assets/Skripts/FieldSpawner.cs
@@ -5,35 +5,107 @@ using UnityEngine.UI;
 
 public class FieldSpawner : MonoBehaviour
 {
-    public float yOffset;
-    public static int fieldWidth, fieldHeight;
+    public Color stdColor;
+    public float yOffset, yOffsetMark;
+    public static int fieldWidth, fieldHeight, markWidth, markHeight;
     private float height, width;
     public Button tilePrefab;
+    public Button markPrefab;
     public static Button[,] field;
+    public static Button[] marker;
+
+    private int activeMarker;
 
     // Start is called before the first frame update
     void Start()
     {
         SetDefaultStats();
-        Spawn();
+        SpawnTiles();
+        SpawnMarks();
     }
 
     private void SetDefaultStats() {
         fieldWidth = 9;
         fieldHeight = 9;
+        markWidth = 5;
+        markHeight = 2;
         field = new Button[fieldWidth, fieldHeight];
+        marker = new Button[markWidth * markHeight];
+        activeMarker = 0;
 
         height = Camera.main.orthographicSize * 2f;
         width = height / Screen.height * Screen.width;
     }
 
-    public void Spawn() { 
+    private void SpawnTiles() {
         // (0,0) is in the lower left corner
-        for(int i = 0; i<fieldWidth; i++) { 
-            for(int j = 0; j<fieldHeight; j++) {
-                Vector3 pos = new Vector3((i-fieldWidth/2)*width/fieldWidth, yOffset + height/2f + (j-fieldWidth)*width/fieldWidth, 0);
+        for (int i = 0; i < fieldWidth; i++)
+        {
+            for (int j = 0; j < fieldHeight; j++)
+            {
+                Vector3 pos = new Vector3((i - fieldWidth / 2) * width / fieldWidth, yOffset + height / 2f + (j - fieldWidth) * width / fieldWidth, 0);
                 field[i, j] = Instantiate(tilePrefab, pos, Quaternion.identity, GameObject.Find("Canvas").transform);
+                field[i, j].GetComponentInChildren<Text>().text = "";
+                Vector2Int coordinates = new Vector2Int(i, j);
+                field[i, j].onClick.AddListener(() => markField(coordinates));
             }
         }
+    }
+
+    private void SpawnMarks() {
+        // (0,0) is in the lower left corner
+        for (int i = 0; i < markWidth; i++)
+        {
+            for (int j = 0; j < markHeight; j++)
+            {
+                int idx = i + j * markWidth;
+                Vector3 pos = new Vector3((i - markWidth / 2) * width / markWidth, yOffsetMark + height / 2f - (j - markWidth) * width / markWidth, 0);
+                marker[idx] = Instantiate(markPrefab, pos, Quaternion.identity, GameObject.Find("Canvas").transform);
+                marker[idx].GetComponentInChildren<Text>().text = idx>0 ? idx.ToString() : "X";
+                marker[idx].onClick.AddListener(() => ChangeMarker(idx));
+            }
+        }
+    }
+
+    public void Solve() {
+        int[,] numbers = new int[9, 9];
+        for(int i = 0; i<9; i++)
+        {
+            for(int j = 0; j<9; j++)
+            {
+                if (field[i, j].GetComponentInChildren<Text>().text.Equals("")) {
+                    numbers[i, j] = 0;
+                }
+                else {
+                    numbers[i, j] = int.Parse(field[i, j].GetComponentInChildren<Text>().text);
+                }
+            }
+        }
+        PrintSolution(Solver.Solve(numbers));
+    }
+
+    public void PrintSolution(int[,] solution) { 
+        for(int i  = 0; i<9; i++)
+        {
+            for(int j = 0; j<9; j++)
+            {
+                field[i, j].GetComponentInChildren<Text>().text = solution[i, j].ToString();
+            }
+        }
+    }
+
+    public void ChangeMarker(int newMarker) {
+        activeMarker = newMarker;
+        for(int i = 0; i<marker.Length; i++)
+        {
+            marker[i].GetComponent<Image>().color = Color.red;
+            marker[i].GetComponent<Image>().color = stdColor;
+        }
+        marker[newMarker].GetComponent<Image>().color = Color.cyan;
+    }
+
+    public void markField(Vector2Int coordinates) {
+        Button toMark = field[coordinates.x, coordinates.y];
+        toMark.GetComponentInChildren<Text>().text = activeMarker > 0 ? activeMarker.ToString() : "";
     }
 }
--- Assets/Skripts/Solver.cs
+++ Assets/Skripts/Solver.cs
@@ -2,21 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Solver : MonoBehaviour
+public static class Solver
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private static bool lastStep;
+
+    public static int[,] Solve(int[,] input) {
+        int counttest = 0;
+        int[,] field = input;
+        int count = 0;
+        for(int i = 0; i<9; i++) { 
+            for(int j = 0; j<9; j++) { 
+                if(field[i,j] == 0) {
+                    count++;
+                }
+            }
+        }
+        int[] solution = new int[count];
+        for(int i = 0; i<solution.Length; i++) {
+            solution[i] = 1;
+        }
+
+        while (true) 
+        {
+            counttest++;
+            field = (int[,])input.Clone();
+            count = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (field[i, j] == 0)
+                    {
+                        field[i, j] = solution[count];
+                        count++;
+                    }
+                }
+            }
+            if (IsValid(field))
+            {
+                return field;
+            }
+            // find new possible solution
+            if (solution[0] < 9) solution[0]++;
+            else
+            {
+                if (solution[0] == 9)
+                {
+                    solution[0] = 1;
+                    solution[1]++;
+                }
+            }
+            for (int i = 1; i < solution.Length - 1; i++)
+            {
+                if (solution[i] == 10)
+                {
+                    solution[i] = 1;
+                    solution[i + 1]++;
+                }
+            }
+            //Debug.Log(solution[0] + "  " + solution[1] + "  " + solution[2] + "  " + solution[3]);
+            if (lastStep)
+            {
+                //Debug.Log(counttest);
+                return null;
+            }
+            lastStep = true;
+            for (int i = 0; i < solution.Length; i++)
+            {
+                if (solution[i] != 9)
+                {
+                    lastStep = false;
+                }
+            }
+        } 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public bool IsValid(int[,] field)
+    public static bool IsValid(int[,] field)
     {
         if (field.GetLength(0) != field.GetLength(1) || (field.GetLength(0) != 3 && field.GetLength(0) != 9)) return false;
         if (field.GetLength(0) == 3)
@@ -34,8 +95,8 @@ public class Solver : MonoBehaviour
                 for (int j = 0; j < field.GetLength(1); j++)
                 {
                     // if foundValues of this position is already true, a pair was found, so the field isn't valid
-                    if (foundValues[field[i, j]]) return false;
-                    foundValues[field[i, j]] = true;
+                    if (foundValues[field[i, j]-1]) return false;
+                    foundValues[field[i, j]-1] = true;
                 }
             }
             // if no mistakes found, return true
@@ -55,10 +116,12 @@ public class Solver : MonoBehaviour
                         for (int y = 0; y < 3; y++)
                         {
                             smallField[x, y] = field[x + xOffset, y + yOffset];
+                            //Debug.Log(smallField[x,y]);
                         }
                     }
                     // return false if one of the small fields is not valid
                     if (!IsValid(smallField)) return false;
+                    Debug.Log("small ok");
                 }
             }
 
@@ -73,8 +136,8 @@ public class Solver : MonoBehaviour
             {
                 for (int y = 0; y < field.GetLength(1); y++)
                 {
-                    if (foundValues[field[x, y]]) return false;
-                    foundValues[field[x, y]] = true;
+                    if (foundValues[field[x, y]-1]) return false;
+                    foundValues[field[x, y]-1] = true;
                 }
                 // initialize the foundValues with false again to start with a new row
                 for (int i = 0; i < foundValues.Length; i++)
@@ -82,6 +145,7 @@ public class Solver : MonoBehaviour
                     foundValues[i] = false;
                 }
             }
+            Debug.Log("rows ok");
 
             // Check if all columns are IsValid
             // At the start, no values are visited; initialize the whole array with false
@@ -93,8 +157,8 @@ public class Solver : MonoBehaviour
             {
                 for (int x = 0; x < field.GetLength(0); x++)
                 {
-                    if (foundValues[field[x, y]]) return false;
-                    foundValues[field[x, y]] = true;
+                    if (foundValues[field[x, y]-1]) return false;
+                    foundValues[field[x, y]-1] = true;
                 }
                 // initialize the foundValues with false again to start with a new row
                 for (int i = 0; i < foundValues.Length; i++)
